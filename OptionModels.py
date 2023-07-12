518b44a054fabcc99e80346f1a966ffca122eb3e
sntfme <74415496+sntfme@users.noreply.github.com>
2023-07-12 17:59:23 +0200

    Added Garman-Kohlhagen model

--- OptionModels.py
+++ OptionModels.py
@@ -10,11 +10,12 @@ class BlackScholes:
             # market input
             self.S, self.K, self.V, self.r, self.type = S, K, V, r, opt
             self.T, self.t = T/365, t/365
+            self.tau = self.T - self.t
             # d1d2
-            self.d1= (np.log(self.S/self.K) + (self.r + (self.V**2)/2)*(self.T - self.t))\
-            /(self.V*np.sqrt(self.T - self.t))
+            self.d1= (np.log(self.S/self.K) + (self.r + (self.V**2)/2)*self.tau)\
+            /(self.V*np.sqrt(self.tau))
 
-            self.d2= self.d1 - self.V*np.sqrt(self.T - self.t)
+            self.d2= self.d1 - self.V*np.sqrt(self.tau)
             # value and greeks
             self.value= value
             self.delta= None
@@ -35,11 +36,11 @@ class BlackScholes:
     def value(self, option):
         if option.type == 'call':    
             valueCall = option.S * norm.cdf(option.d1, 0, 1) - \
-                option.K * np.exp(-option.r*(option.T - option.t)) * norm.cdf(option.d2, 0, 1)
+                option.K * np.exp(-option.r * option.tau) * norm.cdf(option.d2, 0, 1)
             option.value= valueCall
             return valueCall
         elif option.type == 'put':
-            valuePut = option.K * np.exp(-option.r*(option.T - option.t)) * norm.cdf(-option.d2, 0, 1) - \
+            valuePut = option.K * np.exp(-option.r * option.tau) * norm.cdf(-option.d2, 0, 1) - \
                 option.S * norm.cdf(-option.d1, 0, 1)
             option.value= valuePut
             return valuePut
@@ -55,34 +56,34 @@ class BlackScholes:
             return deltaPut
 
     def gamma(self, option):
-        gamma= norm.pdf(option.d1, 0, 1)/(option.S*option.V*np.sqrt(option.T - option.t))
+        gamma= norm.pdf(option.d1, 0, 1)/(option.S*option.V*np.sqrt(option.tau))
         option.gamma= gamma
         return gamma
     
     def vega(self, option):
-        vega= option.S * norm.pdf(option.d1, 0, 1) * np.sqrt(option.T - option.t)
+        vega= option.S * norm.pdf(option.d1, 0, 1) * np.sqrt(option.tau)
         option.vega= vega/100
         return vega/100
     
     def theta(self, option):
         if option.type == 'call':
-            thetaCall= -(option.S * norm.pdf(option.d1, 0, 1) * option.V)/(2*np.sqrt(option.T - option.t)) \
-                  - option.r * option.K * np.exp(-option.r*(option.T - option.t)) * norm.cdf(option.d2, 0, 1)
+            thetaCall= -(option.S * norm.pdf(option.d1, 0, 1) * option.V)/(2*np.sqrt(option.tau)) \
+                  - option.r * option.K * np.exp(-option.r * option.tau) * norm.cdf(option.d2, 0, 1)
             option.theta= thetaCall/365
             return thetaCall/365
         elif option.type == 'put':
-            thetaPut= -(option.S * norm.pdf(option.d1, 0, 1) * option.V/(2*np.sqrt(option.T - option.t))) \
-                  + option.r * option.K * np.exp(-option.r*(option.T - option.t)) * norm.cdf(-option.d2, 0, 1)
-            option.theta= thetaCall/365
+            thetaPut= -(option.S * norm.pdf(option.d1, 0, 1) * option.V/(2*np.sqrt(option.tau))) \
+                  + option.r * option.K * np.exp(-option.r * option.tau) * norm.cdf(-option.d2, 0, 1)
+            option.theta= thetaPut/365
             return thetaPut/365
 
     def rho(self, option):
         if option.type == 'call':
-            rhoCall= option.K * (option.T - option.t) * np.exp(-option.r*(option.T - option.t)) * norm.cdf(option.d2, 0, 1)
+            rhoCall= option.K * option.tau * np.exp(-option.r * option.tau) * norm.cdf(option.d2, 0, 1)
             option.rho= rhoCall/100
             return rhoCall/100
         elif option.type == 'put':
-            rhoPut= -option.K * (option.T - option.t) * np.exp(-option.r*(option.T - option.t)) * norm.cdf(-option.d2, 0, 1)
+            rhoPut= -option.K * option.tau * np.exp(-option.r * option.tau) * norm.cdf(-option.d2, 0, 1)
             option.rho= rhoPut/100
             return rhoPut/100
 
@@ -90,13 +91,15 @@ class GarmanKohlhagen:
     class OptionGK:
         def __init__(self, S= None, K= None, V= None, rd= None, rf= None, T= None, t=0, opt= 'call', value= None):
             # market input
-            self.S, self.K, self.V, self.rd, self.rf, self.opt = S, K, V, rd, rf, opt
+            self.S, self.K, self.V, self.rd, self.rf, self.type = S, K, V, rd, rf, opt
             self.T, self.t = T/365, t/365
-            # d1d2
-            self.d1= (np.log(self.S/self.K) + (self.r + (self.V**2)/2)*(self.T - self.t))\
-            /(self.V*np.sqrt(self.T - self.t))
+            self.tau = self.T - self.t
 
-            self.d2= self.d1 - self.V*np.sqrt(self.T - self.t)
+            # d1d2
+            self.d1 = (np.log(self.S/self.K) + (self.rd - self.rf + (self.V**2)/2)*self.tau)\
+            /(self.V*np.sqrt(self.tau))
+
+            self.d2 = self.d1 - self.V*np.sqrt(self.tau)
             # value and greeks
             self.value= value
             self.delta= None
@@ -116,28 +119,77 @@ class GarmanKohlhagen:
         self.rho_d(self.option)
         self.rho_f(self.option)
 
-    def value(self):
-        pass
+    def value(self, option):
+        if option.type == 'call':    
+            valueCall = option.S * np.exp(-option.rf*option.tau) * norm.cdf(option.d1, 0, 1)\
+                - option.K * np.exp(-option.rd*option.tau) * norm.cdf(option.d2, 0, 1)
+            option.value= valueCall
+            return valueCall
+        elif option.type == 'put':
+            valuePut = option.K * np.exp(-option.rd*option.tau) * norm.cdf(-option.d2, 0, 1)\
+                - option.S * np.exp(-option.rf*option.tau) * norm.cdf(-option.d1, 0, 1)
+            option.value= valuePut
+        return valuePut
 
-    def delta(self):
-        pass
+    def delta(self, option):
+        if option.type == 'call':
+            deltaCall= np.exp(-option.rf*option.tau) * norm.cdf(option.d1, 0, 1)
+            option.delta= deltaCall
+            return deltaCall
+        elif option.type == 'put':
+            deltaPut= np.exp(-option.rf*option.tau) * norm.cdf(option.d1, 0, 1) - 1
+            option.delta= deltaPut
+            return deltaPut
 
-    def gamma(self):
-        pass   
+    def gamma(self, option):
+        gamma= (np.exp(-option.rf*option.tau) * norm.pdf(option.d1, 0, 1))\
+            /(option.S*option.V*np.sqrt(option.tau))
+        option.gamma= gamma
+        return gamma   
 
-    def vega(self):
-        pass
+    def vega(self, option):
+        vega= np.exp(-option.rf*option.tau) * option.S * norm.pdf(option.d1, 0, 1) * np.sqrt(option.tau)
+        option.vega= vega/100
+        return vega/100
 
-    def theta(self):
-        pass
+    def theta(self, option):
+        if option.type == 'call':
+            thetaCall= option.S * option.rf * np.exp(-option.rf*option.tau) * norm.cdf(option.d1, 0, 1)\
+                - option.K * option.rd * np.exp(-option.rd*option.tau) * norm.cdf(option.d2, 0, 1)\
+                - (option.S * option.V * np.exp(-option.rf*option.tau) * norm.pdf(option.d1, 0, 1))/(2*np.sqrt(option.tau))
+            option.theta= thetaCall/365
+            return thetaCall/365
+        elif option.type == 'put':
+            thetaPut= - option.S * option.rf * np.exp(-option.rf*option.tau) * norm.cdf(-option.d1, 0, 1)\
+                + option.K * option.rd * np.exp(-option.rd*option.tau) * norm.cdf(-option.d2, 0, 1)\
+                - (option.S * option.V * np.exp(-option.rf*option.tau) * norm.pdf(option.d1, 0, 1))/(2*np.sqrt(option.tau))
+            option.theta= thetaPut/365
+            return thetaPut/365
 
-    def rho_d(self):
-        pass
+    def rho_d(self, option):
+        if option.type == 'call':
+            rhoCall= option.K * option.tau * np.exp(-option.rd * option.tau) * norm.cdf(option.d2, 0, 1)
+            option.rho_d= rhoCall/100
+            return rhoCall/100
+        elif option.type == 'put':
+            rhoPut= - option.K * option.tau * np.exp(-option.rd * option.tau) * norm.cdf(-option.d2, 0, 1)
+            option.rho_d= rhoPut/100
+            return rhoPut/100
 
-    def rho_f(self):
-        pass 
+    def rho_f(self, option):
+        if option.type == 'call':
+            rhoCall= - option.S * option.tau * np.exp(-option.rf * option.tau) * norm.cdf(option.d1, 0, 1)
+            option.rho_f= rhoCall/100
+            return rhoCall/100
+        elif option.type == 'put':
+            rhoPut= option.S * option.tau * np.exp(-option.rf * option.tau) * norm.cdf(-option.d1, 0, 1)
+            option.rho_f= rhoPut/100
+            return rhoPut/100 
 
 class Heston:
+    pass
+
+class CoxRossRubenstein:
     pass
 
 class ImpliedVolatility:
@@ -177,7 +229,8 @@ class ImpliedVolatility:
         #     _sigma += diff/vega
         # return _sigma
 
-if __name__ == '__main__':
+# BS smoke test
+if __name__ == '__main__' and False:
     spot=90.5
     strike= 92.0
     vol= 0.19
@@ -187,16 +240,58 @@ if __name__ == '__main__':
 
     bs= BlackScholes(S= spot, K= strike, V= vol, r= r, T= T, opt= opt)
     print(f'''
-    <Option>
-    
+    Reference:
+    Spot:   {bs.option.S}    Strike: {bs.option.K}
+    Vol:    {bs.option.V}    Tau:{round(bs.option.tau,4)}
+    R:      {bs.option.r}         
+
+    <Option>  
+
     Type:  {bs.option.type.upper()}
     Value: {round(bs.option.value, 3)}
 
     <Greeks>
 
-    Delta:  {round(bs.option.delta,3)}
-    Gamma:  {round(bs.option.gamma,3)}
-    Vega:   {round(bs.option.vega,3)}
-    Theta:  {round(bs.option.theta,3)}
-    Rho:    {round(bs.option.rho,3)}
+    Delta:      {round(bs.option.delta,3)}
+    Gamma:      {round(bs.option.gamma,3)}
+    Vega:       {round(bs.option.vega,3)}
+    Theta:      {round(bs.option.theta,3)}
+    Rho:        {round(bs.option.rho,3)}
     ''')
+
+# GK smoke test
+if __name__ == '__main__' and False:
+    spot= 90.5
+    strike= 92.0
+    vol= 0.19
+    rd= 0.053
+    rf= 0.025
+    T= 85
+    opt= 'call'
+
+    gk= GarmanKohlhagen(S= spot, K= strike, V= vol, rd= rd, rf= rf, T= T, opt= opt)
+
+    print(f'''
+    Reference:
+    Spot:   {gk.option.S}    Strike: {gk.option.K}
+    Vol:    {gk.option.V}    Tau:{round(gk.option.tau,4)}
+    R_d:    {gk.option.rd}   R_f: {gk.option.rf}      
+
+    <Option>  
+
+    Type:  {gk.option.type.upper()}
+    Value: {round(gk.option.value, 3)}
+
+    <Greeks>
+
+    Delta:      {round(gk.option.delta,3)}
+    Gamma:      {round(gk.option.gamma,3)}
+    Vega:       {round(gk.option.vega,3)}
+    Theta:      {round(gk.option.theta,3)}
+    Rho dmst:   {round(gk.option.rho_d,3)}
+    Rho frgn:   {round(gk.option.rho_f,3)}
+    ''')
+
+
+    # import inspect
+    # print(inspect.getmembers(gk.option))
